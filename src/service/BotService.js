d797f3473b768481aaee71ae0280d7b23bce619f
customwebtemplatedesigner <68409068+customwebtemplatedesigner@users.noreply.github.com>
2020-09-07 13:10:51 -0700

    almost done

--- src/service/BotService.js
+++ src/service/BotService.js
@@ -1,6 +1,11 @@
-const colors = require('colors')
-const Snoowrap = require('snoowrap')
-const requester = require('../config/snoo-config').actionRequester
+const
+    colors = require('colors'),
+    Snoowrap = require('snoowrap'),
+    requester = require('../config/snoo-config').actionRequester,
+    dateFormat = require('dateformat'),
+    scoreIncrement = require('../util/scoreIncrement')
+let promiseObj = {}
+
 
 // This is where your code will go. It must return a promise!
 // This function is called each time a new message is popped from the array.
@@ -8,51 +13,226 @@ const requester = require('../config/snoo-config').actionRequester
 // The InboxStreamGenerator will always give you the first 5 mentions - unread or not
 // So be sure to be save-checking saving and every time!
 async function doSomething(item) {
+
+
     // Checking if the item was saved will keep the bot from processing anything twice.
     if (!item.saved) {
-        console.log(`processing item: ${item.id}`)
-        console.dir(item)
-        //
-        // CODE STARTS HERE //
+        // Show date/time
+        let d = new Date();
+        formattedDate = dateFormat(d)
+        console.log(`\n--------------------${formattedDate}--------------------`.blue)
+        console.log(
+            `\n\tFrom: u/${item.author.name}\n\tRequest: ${item.body}`.grey +'\n'
+        )
 
+        // Process the item
+        await checkMasterSub(item)
+            .then((item) => checkTopLevel(item))
+            .then((promiseObj) => getReleventItem(promiseObj))
+            .then((releventItem) => designateFlairs(releventItem))
+            .then((msg) => replyWithMessage(msg))
+            .then(() => saveItem(item))
+            .catch((err) => {
+                if (err.id != 0) {
+                    replyWithMessage(err)
+                        .then(() => saveItem(item))
+                } else {
+                    console.log(msg.message)
+                }
+            })
 
-
-
-        await printContents(item)
-        await replyToComment(item)
-
+        return console.log(`item '${item.id}' successfully processed!`.green)
 
 
 
 
         // CODE ENDS HERE //
-        //
-        // Be sure to finish with saveItem(item) so the item will not be processed again.
-        await saveItem(item) // Leave this line where it is!
-        return console.log(`item  ${item.id} successfully processed!`)
+
+
     } else {
-        return console.log(`item ${item.id} saved already. Skipping...`)
+        return console.log(`item '${item.id}' already saved. skipping.`.grey)
+    }
+}
+//// PlayingCardsMarket Bot
+///
+//
+
+
+const MASTER_SUB = process.env.MASTER_SUB;
+// 1. Check that mention received is from MASTER_SUB
+const checkMasterSub = function (item) {
+    // If mention from master sub, continue processing
+    if (item.subreddit_name_prefixed === 'r/' + MASTER_SUB && item.parent_id != null) {
+        return Promise.resolve(item)
+
+        // else reject with error message
+    } else {
+        console.log(`was from ${item.subreddit_name_prefixed}. Rejecting!`.red)
+        err = {
+            id: 0,
+            message: "Attempted request from unauthorized sub."
+
+        }
+        return Promise.reject(err)
     }
 }
 
-// 
-// Write your functions down here and call them inside of doSomething.
-const printContents = function (item) {
-    console.log(('printing contents of item.body:\n ' + item.body).green)
-    return Promise.resolve(item)
+// 2. Check if top level or comment reply
+const checkTopLevel = function (item) {
+    const regexP = RegExp(/^t3_/)
+    promiseObj.mention = item;
+    promiseObj.wasTopLevel = regexP.test(item.parent_id);
+    promiseObj.mention.parent_id = splitParentId(item.parent_id);
+
+    return Promise.resolve(promiseObj)
+
 }
-const replyToComment = function (item) {
-    console.log("replying to comment...".grey)
-    return requester.getComment(item.id).reply("I just replied!")
+
+// 2-A Split Parent ID
+const splitParentId = function (id) {
+    let splitResult = id.split('_')[1];
+    return splitResult;
 }
-// 
+
+// 3. Get Comment OR Submission
+const releventItem = {}
+const getReleventItem = async function (promiseObj) {
+    parent_id = promiseObj.mention.parent_id
+    wasTopLevel = promiseObj.wasTopLevel
+    // If was top level:
+    if (promiseObj.wasTopLevel) {
+        const submission = await fetchSubmission(promiseObj.mention.parent_id)
+        releventItem.submission = submission
+        releventItem.childComment = promiseObj.mention
+
+        return Promise.resolve(releventItem)
+
+        // If was not top level:
+    } else if (!promiseObj.wasTopLevel) {
+        const comment = await fetchComment(promiseObj.mention.parent_id)
+
+        releventItem.submission = comment
+        releventItem.childComment = promiseObj.mention
+
+        return Promise.resolve(releventItem)
+
+    }
+
+}
+
+// 3-A Fetch the Submission
+const fetchSubmission = function (id) {
+    return requester.getSubmission(id).fetch()
+}
+// 3-B Fetch The Comment
+const fetchComment = function (id) {
+    return requester.getComment(id).fetch()
+}
+
+
+// 4. Assign Flairs OR Reject the Request
+async function designateFlairs(releventItem) {
+    console.log(`referencing u/${releventItem.submission.author.name}`.yellow)
+
+    if (releventItem.submission.author.name === releventItem.childComment.author.name) {
+        console.log('user attempted to vote on self!'.red)
+        err = {
+            id: releventItem.childComment.id,
+            message: "Nice try! You can't vote on yourself!"
+        }
+        return Promise.reject(err)
+    }
+
+    console.log('NO ERROR FOUND... CONTINUEING'.green)
+    let author_flair = String;
+    console.log(`assigning flairs...`.magenta)
+    directive = promiseObj.mention.body
+
+    // If no direcive, ignore request.
+    if (!directive.includes('!neutral') && !directive.includes('!positive') && !directive.includes('!negative')) {
+
+        message =
+            `You need to give me a direction!  
+            
+            To cast your vote successfully:  
+            
+            u/${process.env.REDDIT_USER} !positive
+            u/${process.env.REDDIT_USER} !neutral
+            u/${process.env.REDDIT_USER} !negative`
+
+        console.log('DID NOT INCLUDE A DIRECTIVE. REJECTING WITH MESSAGE...'.red)
+        err = {
+            id: releventItem.childComment.id,
+            message: message
+        }
+        return Promise.reject(err)
+
+    }
+
+
+    // else, designate the flairs
+    console.log(`getting current user flair of u/${releventItem.submission.author.name}`.green)
+    const userFlair = await requester.getSubreddit(process.env.MASTER_SUB).getUserFlair(releventItem.submission.author.name)
+
+    flair = userFlair.flair_text;
+
+    if (flair == undefined | flair == '') {
+        author_flair = 'Positive: 0 Neutral: 0 Negative: 0'
+    } else {
+        author_flair = flair
+    }
+
+    if (directive.includes('!positive')) {
+        console.log(`incrementing positive count`.green)
+        newFlair = scoreIncrement.incrementPositiveCount(author_flair)
+    }
+
+    if (directive.includes('!negative')) {
+        console.log(`incrementing negative count`.red)
+        newFlair = scoreIncrement.incrementNegativeCount(author_flair)
+    }
+    if (directive.includes('!neutral')) {
+        console.log('incrementing neutral count'.grey)
+        newFlair = scoreIncrement.incrementNeutralCount(author_flair)
+    }
+
+    message = 'Thanks for your input! Your vote has been accounted for.'
+
+    await assignFlair(releventItem.submission.author.name, newFlair)
+
+    console.log(`sucessfully updated user flair!`.yellow)
+    msg = {
+        id: releventItem.childComment.id,
+        message: message
+
+    }
+    return Promise.resolve(msg)
+
+}
+
+async function assignFlair(author, flair) {
+    console.log(`assigning updated flair: (${flair}) to u/${author}`.yellow)
+
+    const userFlair = await requester.getUser(author).assignFlair({
+        subredditName: MASTER_SUB,
+        text: flair,
+        cssClass: process.env.FLAIR_CSS_CLASS
+    })
+    return Promise.resolve(userFlair)
+}
+
+const replyWithMessage = function (msg) {
+    return requester.getComment(msg.id).reply(msg.message);
+
+}
+
 
 
 
 // 
 // Leave this function alone!
 const saveItem = function (item) {
-    console.log("saving the comment...".grey)
+    console.log(`saving the comment...`.magenta)
     return requester.getComment(item.id).save();
 }
 module.exports = {
